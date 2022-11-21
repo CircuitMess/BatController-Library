083137bdf80aaf21475edb1008d953eb6f632b92
subo <gamedevsubo@gmail.com>
2022-11-21 15:07:50 +0100

    Add soundVolume to SettingsData

--- src/Settings.h
+++ src/Settings.h
@@ -1,5 +1,5 @@
-#ifndef CHATTER_LIBRARY_SETTINGS_H
-#define CHATTER_LIBRARY_SETTINGS_H
+#ifndef BATCTRL_LIBRARY_SETTINGS_H
+#define BATCTRL_LIBRARY_SETTINGS_H
 
 #include <Arduino.h>
 
@@ -9,6 +9,7 @@ extern const uint32_t ShutdownSeconds[SHUTDOWN_STEPS];
 extern const char* ShutdownText[SHUTDOWN_STEPS];
 
 struct SettingsData {
+	uint8_t soundVolume = 100;
 	uint32_t shutdownTime = 2;
 	uint8_t screenBrightness = 200;
 	bool hwTested = false;
@@ -30,4 +31,4 @@ private:
 
 extern SettingsImpl Settings;
 
-#endif //CHATTER_LIBRARY_SETTINGS_H
+#endif //BATCTRL_LIBRARY_SETTINGS_H
