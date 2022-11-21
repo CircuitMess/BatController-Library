f9f40a99b204f2d64288526cf3aedeb31782e5ca
subo <gamedevsubo@gmail.com>
2022-11-21 15:07:17 +0100

    Add shutdown text

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,6 +3,9 @@
 
 SettingsImpl Settings;
 
+const uint32_t ShutdownSeconds[SHUTDOWN_STEPS] = { 0, 60*60, 6 * 60*60, 24 * 60*60, 2 * 24 * 60*60 };
+const char* ShutdownText[SHUTDOWN_STEPS] = { "OFF", "1 hour", "6 hours", "1 day", "2 days" };
+
 bool SettingsImpl::begin(){
 	return Settings::init(&data, sizeof(SettingsData));
 }
--- src/Settings.h
+++ src/Settings.h
@@ -5,6 +5,9 @@
 
 #define SHUTDOWN_STEPS 5
 
+extern const uint32_t ShutdownSeconds[SHUTDOWN_STEPS];
+extern const char* ShutdownText[SHUTDOWN_STEPS];
+
 struct SettingsData {
 	uint32_t shutdownTime = 2;
 	uint8_t screenBrightness = 200;
