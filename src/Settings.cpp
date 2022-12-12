feccacdf36be891ed70479e004d0aad852302cc6
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2022-12-12 15:56:04 +0100

    Settings bat 69 (#5)
    * Add shutdown text
    
    * Add soundVolume to SettingsData
    
    * Adjust shutdown times
    
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,6 +3,9 @@
 
 SettingsImpl Settings;
 
+const uint32_t ShutdownSeconds[SHUTDOWN_STEPS] = { 0, 60, 5 * 60, 10 * 60, 15 * 60 };
+const char* ShutdownText[SHUTDOWN_STEPS] = { "OFF", "1 min", "5 min", "10 min", "15 min" };
+
 bool SettingsImpl::begin(){
 	return Settings::init(&data, sizeof(SettingsData));
 }
--- src/Settings.h
+++ src/Settings.h
@@ -1,11 +1,15 @@
-#ifndef CHATTER_LIBRARY_SETTINGS_H
-#define CHATTER_LIBRARY_SETTINGS_H
+#ifndef BATCTRL_LIBRARY_SETTINGS_H
+#define BATCTRL_LIBRARY_SETTINGS_H
 
 #include <Arduino.h>
 
 #define SHUTDOWN_STEPS 5
 
+extern const uint32_t ShutdownSeconds[SHUTDOWN_STEPS];
+extern const char* ShutdownText[SHUTDOWN_STEPS];
+
 struct SettingsData {
+	uint8_t soundVolume = 100;
 	uint32_t shutdownTime = 2;
 	uint8_t screenBrightness = 200;
 	bool hwTested = false;
@@ -27,4 +31,4 @@ private:
 
 extern SettingsImpl Settings;
 
-#endif //CHATTER_LIBRARY_SETTINGS_H
+#endif //BATCTRL_LIBRARY_SETTINGS_H
